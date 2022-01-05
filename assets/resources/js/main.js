8c5ce62ebe599788630411b68a6494ee7eeecbab
Mazharul68 <eng.mazharulfiverr@yahoo.com>
2022-01-05 22:13:01 +0600

    slider added

--- assets/resources/js/main.js
+++ assets/resources/js/main.js
@@ -29,4 +29,51 @@ $(document).ready(function() {
         $('.navbar i').toggleClass("active");
     });
 
+
+
+    //Typing Scripts
+    var typed = new Typed(".tying", {
+        strings: ["ready to give maximum service", "by your side in your service "],
+        typeSpeed: 100,
+        backSpeed: 10,
+        loop: true
+    });
+
+    //Typing Scripts
+    var typed = new Typed(".tying2", {
+        strings: ["specialist", "CARDIOLOGY", "DENTAL UNIT"],
+        typeSpeed: 150,
+        backSpeed: 10,
+        loop: true
+    });
+    //Typing Scripts
+    var typed = new Typed(".tying3", {
+        strings: ["CRITICAL CARE MEDICINE", "DERMATOLOGY", "DIABETOLOGY"],
+        typeSpeed: 150,
+        backSpeed: 10,
+        loop: true
+    });
+    // Owl Coursel Js
+    $('#slider-owl-carousel').owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 20000,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        item: 1,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+
 });
